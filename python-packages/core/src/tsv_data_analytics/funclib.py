c4fc3c7b68c2331f89f85e155c5910c6f86db914
Amit Jaiswal <amit.jaiswal@crowdstrike.com>
2021-11-25 12:07:38 -0800

    deprecating method

--- python-packages/core/src/tsv_data_analytics/funclib.py
+++ python-packages/core/src/tsv_data_analytics/funclib.py
@@ -130,7 +130,7 @@ def get_str_map_without_keys(mp, excluded_keys):
 
 
 # TODO
-def datetime_to_timestamp(x):
+def datetime_to_utctimestamp(x):
     if (x.endswith("UTC") or x.endswith("GMT") or x.endswith("Z") or x.endswith("+00:00")):
         return int(parser.parse(x).timestamp())
     elif (len(x) == 10):
@@ -147,4 +147,7 @@ def datetime_to_timestamp(x):
         return int(parser.parse(x).timestamp())
     else:
         raise Exception("Unknown date format. Problem with UTC: {}".format(x))
+
+def datetime_to_timestamp(x):
+    raise Exception("Please use datetime_to_utctimestamp")
 
