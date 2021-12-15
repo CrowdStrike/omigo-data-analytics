1f424fa969807c4c50a7b402bc5f7e8f1417c2ac
Amit Jaiswal <amitjaiswal@users.noreply.github.com>
2021-12-15 14:10:04 -0800

    Updating docker file and notebook

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -3,5 +3,10 @@
 BASEDIR="/code"
 
 cd $BASEDIR
+
+echo "Installing any updated version of the packages"
+pip3 install tsv_data_analytics tsv_data_analytics_ext --upgrade
+
+echo "Running jupyter"
 jupyter lab --ip 0.0.0.0 --port 8888 --allow-root
 
