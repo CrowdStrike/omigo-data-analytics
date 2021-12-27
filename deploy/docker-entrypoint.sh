56f9892dce08ad6cb127a64617771b40ad731ecf
Amit Jaiswal <amit.jaiswal@crowdstrike.com>
2021-12-27 12:53:19 -0800

    Updating docker files

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -5,7 +5,7 @@ BASEDIR="/code"
 cd $BASEDIR
 
 echo "Installing any updated version of the packages"
-pip3 install tsv_data_analytics tsv_data_analytics_ext --upgrade
+pip3 install omigo_core omigo_ext --upgrade
 
 echo "Running jupyter"
 jupyter lab --ip 0.0.0.0 --port 8888 --allow-root
