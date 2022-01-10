741e36f0eeb18300172c941323517ef06958c791
Amit Jaiswal <amit.jaiswal@crowdstrike.com>
2022-01-09 18:33:05 -0800

    updating docker run

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -4,8 +4,8 @@ BASEDIR="/code"
 
 cd $BASEDIR
 
-echo "Installing any updated version of the packages"
-./install.sh
+# echo "Installing any updated version of the packages"
+# ./install.sh
 
 echo "Running jupyter"
 jupyter lab --ip 0.0.0.0 --port 8888 --allow-root
