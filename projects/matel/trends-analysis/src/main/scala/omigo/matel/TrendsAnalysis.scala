64993b0227f829c209e9de2a1d8d77965750cae7
Amit Jaiswal <amit.jaiswal@gmail.com>
2023-06-04 23:37:23 -0700

    Working version

--- projects/matel/trends-analysis/src/main/scala/omigo/matel/TrendsAnalysis.scala
+++ projects/matel/trends-analysis/src/main/scala/omigo/matel/TrendsAnalysis.scala
@@ -27,22 +27,24 @@ import org.joda.time.DateTimeZone
 import org.joda.time.format.DateTimeFormat
 import scala.reflect.ClassTag
 
+import omigo_core.TSV
+
 object TrendsAnalysis {
   def main(args: Array[String]): Unit = {
     // Check command line parameters
     if (args.length != 3) {
-        println("Usage: TrendsAnalysis <config-file> <input-dir> <output-dir>")
+        println("Usage: TrendsAnalysis <config-file> <input-file> <output-dir>")
         return
     }
 
     // read command line parameters
     val propsFile = args(0)
-    val inputDir = args(1)
+    val inputFile = args(1)
     val outputDir = args(2)
 
     // Debug
     println("Parameter: propsFile: " + propsFile)
-    println("Parameter: inputDir:  " + inputDir)
+    println("Parameter: inputFile:  " + inputFile)
     println("Parameter: outputDir: " + outputDir)
 
     // read configuration file
@@ -51,5 +53,65 @@ object TrendsAnalysis {
 
     // Debug
     println("Properties: " + props)
+
+    // initialize spark session
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .enableHiveSupport()
+      .appName("TrendsAnalysis")
+      .getOrCreate()
+
+    // run different steps
+
+    // 1. read input data from lokki library and write it back in parquet
+    readBaseData(spark, inputFile, outputDir + "/base", props)
+
+    // 2. run custom enrichment step
+    runEnrichment(spark, outputDir + "/base", outputDir + "/enrichment")
+
+    // 3. create ancestry
+    runAncestry(spark, outputDir + "/enrichment", outputDir + "/ancestry")
+
+    // 4. group events
+    createGroups(spark, outputDir + "/ancestry", outputDir + "/groups")
+
+    // 5. generate different kinds of dictionaries
+    createDicts(spark, outputDir + "/groups", outputDir + "/dicts")
+  }
+
+  def readBaseData(spark: SparkSession, inputFile: String, outputDir: String, props: Properties) = {
+    import spark.implicits._
+
+    // read the tsv file
+    val xtsv = TSV.read(inputFile, "\t")
+
+    // read the data
+    val cols = Seq("id_level1", "id_level2", "uid", "node_id", "parent_id")
+    println(cols)
+
+    val tuples = xtsv.to_tuples5(cols, "readBaseData")
+    // println(tuples)
+
+    // create parquet file with all necessary columns
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(tuples)).toDF(cols:_*)
+    df.show()
+    // tuples
+  }
+
+  def runEnrichment(spark: SparkSession, inputDir: String, outputDir: String): Unit = {
+
+  }
+
+  def runAncestry(spark: SparkSession, inputDir: String, outputDir: String): Unit = {
+
+  }
+
+  def createGroups(spark: SparkSession, inputDir: String, outputDir: String): Unit = {
+
+  }
+
+  def createDicts(spark: SparkSession, inputDir: String, outputDir: String): Unit = {
+
   }
 }
