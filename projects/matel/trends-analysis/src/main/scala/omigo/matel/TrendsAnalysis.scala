4167de601567ab9235aa8979eccf9e627c8f5dfb
Amit Jaiswal <amit.jaiswal@gmail.com>
2023-06-10 13:35:12 -0700

    Updating with warning

--- projects/matel/trends-analysis/src/main/scala/omigo/matel/TrendsAnalysis.scala
+++ projects/matel/trends-analysis/src/main/scala/omigo/matel/TrendsAnalysis.scala
@@ -60,6 +60,7 @@ object TrendsAnalysis {
       .master("local")
       .enableHiveSupport()
       .appName("TrendsAnalysis")
+      .config("spark.yarn.maxAppAttempts", "1")
       .getOrCreate()
 
     // run different steps
@@ -71,7 +72,8 @@ object TrendsAnalysis {
     runEnrichment(spark, outputDir + "/base", outputDir + "/enrichment")
 
     // 3. create ancestry
-    runAncestry(spark, outputDir + "/enrichment", outputDir + "/ancestry")
+    val numLevels = 6
+    createHierarchy(spark, outputDir + "/enrichment", numLevels, outputDir + "/ancestry")
 
     // 4. group events
     createGroups(spark, outputDir + "/ancestry", outputDir + "/groups")
@@ -88,7 +90,7 @@ object TrendsAnalysis {
     val xtsv = TSV.read(inputFile, "\t")
 
     // read the data
-    val cols = Seq("id_level1", "id_level2", "uid", "node_id", "node_name", "parent_id", "event_name", "event_id", "ts")
+    val cols = Seq("id_level1", "id_level2", "uid", "node_id", "node_label", "parent_id", "event_name", "event_id", "ts")
     println(cols)
 
     val tuples = xtsv.to_tuples9(cols, "readBaseData")
@@ -106,7 +108,7 @@ object TrendsAnalysis {
     import spark.sqlContext.implicits._
 
     // check if already exists
-    if (Utils.checkIfExists(spark, outputPath) == true) {
+    if (false && Utils.checkIfExists(spark, outputPath) == true) {
       println("doEnrichment: outputPath already exists. Skipping ... : " + outputPath)
       return
     }
@@ -115,18 +117,18 @@ object TrendsAnalysis {
     base.createOrReplaceTempView("base")
 
     // base kv
-    val baseKV = spark.sql("select id_level1, id_level2, uid, node_id, node_name, parent_id, event_name, event_id, ts from base").rdd.map({ row =>
+    val baseKV = spark.sql("select id_level1, id_level2, uid, node_id, node_label, parent_id, event_name, event_id, ts from base").rdd.map({ row =>
       val id_level1 = row.getString(row.fieldIndex("id_level1"))
       val id_level2 = row.getString(row.fieldIndex("id_level2"))
       val uid = row.getString(row.fieldIndex("uid"))
       val node_id = row.getString(row.fieldIndex("node_id"))
-      val node_name = row.getString(row.fieldIndex("node_name"))
+      val node_label = row.getString(row.fieldIndex("node_label"))
       val parent_id = row.getString(row.fieldIndex("parent_id"))
       val event_name = row.getString(row.fieldIndex("event_name"))
       val event_id = row.getString(row.fieldIndex("event_id"))
       val ts = row.getString(row.fieldIndex("ts"))
 
-      ((id_level1, id_level2, event_id, ts), (uid, node_id, node_name, parent_id, event_name))
+      ((id_level1, id_level2, event_id, ts), (uid, node_id, node_label, parent_id, event_name))
     })
 
     // read data
@@ -151,18 +153,18 @@ object TrendsAnalysis {
 
     // join
     val joined = (baseKV join dayOfWeekKV).map({
-      case ((id_level1, id_level2, event_id, ts), ((uid, node_id, node_name, parent_id, event_name), (localDateTimeNum, localHour, localDayOfWeek))) =>
+      case ((id_level1, id_level2, event_id, ts), ((uid, node_id, node_label, parent_id, event_name), (localDateTimeNum, localHour, localDayOfWeek))) =>
         val keyValues = List(
           ("local_date_time_num", localDateTimeNum.toString),
           ("local_hour", localHour.toString),
           ("local_day_of_week", localDayOfWeek.toString)
         )
         .toMap
-        (id_level1, id_level2, uid, node_id, node_name, parent_id, event_name, event_id, ts, keyValues)
+        (id_level1, id_level2, uid, node_id, node_label, parent_id, event_name, event_id, ts, keyValues)
     })
 
     // create dataframe with keyvalues
-    val cols = Seq("id_level1", "id_level2", "uid", "node_id", "node_name", "parent_id", "event_name", "event_id", "ts", "keyValues")
+    val cols = Seq("id_level1", "id_level2", "uid", "node_id", "node_label", "parent_id", "event_name", "event_id", "ts", "kv")
     val df = spark.createDataFrame(joined).toDF(cols:_*)
 
     // persist
@@ -170,13 +172,16 @@ object TrendsAnalysis {
     spark.catalog.dropTempView("base")
   }
 
-  def runAncestry(spark: SparkSession, inputPath: String, outputPath: String): Unit = {
+  /**
+   * Method to generate the node hierarchy
+   */
+  def createHierarchy(spark: SparkSession, inputPath: String, numLevels: Int, outputPath: String): Unit = {
     import spark.implicits._
     import spark.sqlContext.implicits._
 
     // check if already exists
-    if (Utils.checkIfExists(spark, outputPath) == true) {
-      println("runAncestry: outputPath already exists. Skipping ... : " + outputPath)
+    if (false && Utils.checkIfExists(spark, outputPath) == true) {
+      println("createHierarchy: outputPath already exists. Skipping ... : " + outputPath)
       return
     }
 
@@ -185,88 +190,134 @@ object TrendsAnalysis {
     base.createOrReplaceTempView("base")
 
     // base kv
-    val baseRDD = spark.sql("select id_level1, id_level2, node_id, node_name, parent_id, ts from base").rdd.map({ row =>
+    val baseRDD = spark.sql("select id_level1, id_level2, node_id, parent_id, node_label, event_id, ts from base").rdd.map({ row =>
       val id_level1 = row.getString(row.fieldIndex("id_level1"))
       val id_level2 = row.getString(row.fieldIndex("id_level2"))
       val node_id = row.getString(row.fieldIndex("node_id"))
-      val node_name = row.getString(row.fieldIndex("node_name"))
       val parent_id = row.getString(row.fieldIndex("parent_id"))
+      val node_label = row.getString(row.fieldIndex("node_label"))
+      val event_id = row.getString(row.fieldIndex("event_id"))
       val ts = row.getString(row.fieldIndex("ts"))
 
       // generate only for valid node ids
-      (id_level1, id_level2, node_id, parent_id, ts.toLong)
+      (id_level1, id_level2, node_id, parent_id, node_label, event_id, ts)
     })
 
     // parent
-    val parentInfo = baseRDD.filter({ case (id_level1, id_level2, node_id, parent_id, ts) => node_id != "" })
-
-    // children
-    val emptyList: List[String] = List.empty
+    val parentInfo = baseRDD
+      .filter({ case (id_level1, id_level2, node_id, parent_id, node_label, event_id, ts) => node_id != "" })
+      .map({ case (id_level1, id_level2, node_id, parent_id, node_label, event_id, ts) =>
+        (id_level1, id_level2, node_id, parent_id, node_label, ts)
+      })
 
     // ancestor level0
-    val ancestorLevel0 = parentInfo.map({ case (id_level1, id_level2, node_id, parent_id, ts) => (id_level1, id_level2, parent_id, event_id, ts, List(node_id)) })
-    val ancestorLevel1 = addOneLevelParent(ancestorLevel0, parentInfo)
-    val ancestorLevel2 = addOneLevelParent(ancestorLevel1, parentInfo)
-    val ancestorLevel3 = addOneLevelParent(ancestorLevel2, parentInfo)
-    val ancestorLevelFinal = ancestorLevel3
+    val ancestorLevelBase = baseRDD.map({
+      case (id_level1, id_level2, node_id, parent_id, node_label, event_id, ts) =>
+        (id_level1, id_level2, event_id, parent_id, ts, List(node_id), List(node_label))
+    })
+
+    // run loop to generate ancestry upto given levels
+    var ancestorLevel = ancestorLevelBase
+    Range(0, numLevels).foreach({ n =>
+      println("createHierarchy: running a round for ancestry: %s".format(n))
+      ancestorLevel = addParent(ancestorLevel, parentInfo)
+    })
 
     // base kv
-    val baseKV = spark.sql("select id_level1, id_level2, uid, node_id, parent_id, event_name, event_id, ts from base").rdd.map({ row =>
+    val baseKV = spark.sql("select * from base").rdd.map({ row =>
       val id_level1 = row.getString(row.fieldIndex("id_level1"))
       val id_level2 = row.getString(row.fieldIndex("id_level2"))
       val uid = row.getString(row.fieldIndex("uid"))
       val node_id = row.getString(row.fieldIndex("node_id"))
       val parent_id = row.getString(row.fieldIndex("parent_id"))
+      val node_label = row.getString(row.fieldIndex("node_label"))
       val event_name = row.getString(row.fieldIndex("event_name"))
       val event_id = row.getString(row.fieldIndex("event_id"))
       val ts = row.getString(row.fieldIndex("ts"))
+      val kv = row.getMap(row.fieldIndex("kv")).asInstanceOf[Map[String, String]]
 
-      ((id_level1, id_level2, event_id, ts), (uid, node_id, parent_id, event_name))
+      ((id_level1, id_level2, event_id, ts), (uid, node_id, parent_id, node_label, event_name, kv))
     })
 
     // take final ancestor level
-    val ancestorLevelKV = ancestorLevelFinal
-      .map({ case (id_level1, id_level2, event_id, level_ppid, ts, ppid_list) =>
-        ((id_level1, id_level2, event_id, ts), ppid_list)
+    val ancestorLevelKV = ancestorLevel
+      .map({ case (id_level1, id_level2, event_id, level_ppid, ts, ppid_list, ppid_label_list) =>
+        ((id_level1, id_level2, event_id, ts), (ppid_list, ppid_label_list))
       })
 
+    // join
     val joined = (baseKV leftOuterJoin ancestorLevelKV)
-      .map({ case ((id_level1, id_level2, event_id, ts), ((uid, node_id, parent_id, event_name), ancestorOpt)) =>
-        val ppid_list = ancestorOpt.getOrElse(List.empty)
+      .map({ case ((id_level1, id_level2, event_id, ts), ((uid, node_id, parent_id, node_label, event_name, kv), ancestorOpt)) =>
+        val (ppid_list, ppid_label_list) = ancestorOpt.getOrElse((List.empty, List.empty))
+
+        // append the new columns to kv
+        val ancestry_kv = List(
+          ("ancestor_ids", ppid_list.filter(_ != "").mkString(",")),
+          ("ancestor_labels", ppid_label_list.filter(_ != "").mkString(",")),
+          ("ancestor_count", ppid_list.length.toString)
+        ).toMap
+
+        (id_level1, id_level2, uid, node_id, parent_id, node_label, event_name, event_id, ts, kv, ancestry_kv)
       })
+
+    // create dataframe
+    val cols = List("id_level1", "id_level2", "uid", "node_id", "parent_id", "node_label", "event_name", "event_id", "ts", "kv", "ancestry_kv")
+    val df = spark.createDataFrame(joined).toDF(cols:_*)
+
+    // persist
+    df.write.mode(SaveMode.Overwrite).parquet(outputPath)
+    spark.catalog.dropTempView("base")
   }
 
-  /*
-   * Method to add one level parent informaton.
+  /**
+   * Method to add one level parent informaton. TODO: The timestamp handling can have side effect, and need proper config parameters
    */
-  def addOneLevelParent(eventLevel: RDD[(String, String, String, String, Long, List[String])], parentInfo: RDD[(String, String, String, String, Long)]) = {
-    val eventLevelKV = eventLevel.map({ case (id_level1, id_level2, level_ppid, event_id, ts, ppid_list) => ((id_level1, id_level2, level_ppid), (event_id, ts, ppid_list)) })
+  def addParent(nodeLevel: RDD[(String, String, String, String, String, List[String], List[String])], parentInfo: RDD[(String, String, String, String, String, String)], numReducers: Int = 10) = {
+    // create event level kv
+    val nodeLevelKV = nodeLevel.map({
+      case (id_level1, id_level2, level_ppid, event_id, ts, ppid_list, ppid_label_list) =>
+        ((id_level1, id_level2, level_ppid), (event_id, ts, ppid_list, ppid_label_list))
+    })
 
-    val eventParentInfoKV = parentInfo
-      .map({ case (id_level1, id_level2, node_id, parent_id, ts) => ((id_level1, id_level2, node_id), (parent_id, ts)) })
-      .filter({ case ((id_level1, id_level2, node_id), (parent_id, ts)) => node_id != null && node_id != "" && node_id != "0"}) // The parents are already non null
-      .groupByKey(1000)
+    // create event parent. TODO custom filtering function
+    val parentInfoKV = parentInfo
+      .map({ case (id_level1, id_level2, node_id, parent_id, node_label, ts) =>
+        ((id_level1, id_level2, node_id), (parent_id, node_label, ts))
+      })
+      // .filter({ case ((id_level1, id_level2, node_id), (parent_id, node_label, ts)) => node_id != null && node_id != ""}) // The parents are already non null
+      .groupByKey(numReducers)
 
-    (eventLevelKV leftOuterJoin eventParentInfoKV)
+    // join
+    val joined = (nodeLevelKV leftOuterJoin parentInfoKV)
       .map({
-        case ((id_level1, id_level2, level_ppid), ((event_id, ts, ppid_list), vsParentOpt)) =>
+        case ((id_level1, id_level2, level_ppid), ((event_id, ts, ppid_list, ppid_label_list), vsParentOpt)) =>
           val vsParent = vsParentOpt.getOrElse(List.empty)
-          val vsParentBefore = vsParent.filter({ case (p_ppid, p_ts) =>
-            ppid_list.contains(p_ppid) == false && (p_ppid != null && p_ppid != "" && p_ppid != "0") &&
-            (p_ts <= ts) // TODO: parent should be before child. is there noise?. Linux recycles pids frequently may be
+          val vsParentBefore = vsParent.filter({ case (p_ppid, p_node_label, p_ts) =>
+            // TODO: parent should be before child. is there noise. Custom filter on invalid parent ids
+            ppid_list.contains(p_ppid) == false // && (p_ppid != null && p_ppid != "")
           })
 
+          // incorporate timestamp filters if present
+          val vsParentBeforeTS = vsParentBefore.filter({ case (p_ppid, p_node_label, p_ts) => (ts == "" || p_ts == "" || p_ts.toLong <= ts.toLong) })
+
           // pick the most recent parent
-          if (vsParentBefore.nonEmpty) {
-            val vParentMax = vsParentBefore.maxBy({ case (p_ppid, p_ts) => p_ts})
-            val (p_ppid, p_ts) = vParentMax
+          if (vsParentBeforeTS.nonEmpty) {
+            // get the most relevant parent based on timestamp or the string sorting of ppid
+            val (p_ppid, p_node_label, p_ts) = {
+              if (ts != "")
+                vsParentBeforeTS.maxBy({ case (p_ppid2, p_node_label2, p_ts2) => p_ts2.toLong })
+              else
+                vsParentBeforeTS.maxBy({ case (p_ppid2, p_node_label2, p_ts2) => p_ppid2 })
+            }
 
             // ts is the timestamp of the event
-            (id_level1, id_level2, event_id, p_ppid, ts, ppid_list ++ List(level_ppid))
+            (id_level1, id_level2, event_id, p_ppid, ts, ppid_list ++ List(level_ppid), ppid_label_list ++ List(p_node_label))
           } else {
-            (id_level1, id_level2, event_id, level_ppid, ts, ppid_list)
+            (id_level1, id_level2, event_id, level_ppid, ts, ppid_list, ppid_label_list)
           }
       })
+
+    joined
   }
 
   def createGroups(spark: SparkSession, inputPath: String, outputPath: String): Unit = {
