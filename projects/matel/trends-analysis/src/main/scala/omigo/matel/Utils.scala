aa5e5b6507cdc4872054172313d30b9aa3e29c18
Amit Jaiswal <amit.jaiswal@gmail.com>
2023-07-15 17:25:52 -0700

    Updating utils for bitset

--- projects/matel/trends-analysis/src/main/scala/omigo/matel/Utils.scala
+++ projects/matel/trends-analysis/src/main/scala/omigo/matel/Utils.scala
@@ -105,3 +105,48 @@ object Utils {
       Map.empty
   }
 }
+
+/**
+ * FilterFunction implementation for bitset filters.
+ */
+class BitSetFilterFunctions(filterBitSetAndMapBC: Broadcast[Map[String, BitSet]], filterBitSetOrMapBC: Broadcast[Map[String, Seq[BitSet]]]) extends FilterFunction[Row] {
+  override def call(row: Row): Boolean = {
+
+    // check for and clause
+    val filter1 = filterBitSetAndMapBC.value.forall({ case (name, filterBitSet) =>
+      val dataBitSet = deserializeBitSet(row.get(row.fieldIndex(name)).asInstanceOf[Array[Byte]])
+      val bitSet = new BitSet()
+      bitSet.or(filterBitSet)
+      bitSet.and(dataBitSet)
+      bitSet.cardinality() == filterBitSet.cardinality()
+    })
+
+    // early return if no match in and clause
+    if (filter1 == false) {
+      return false
+    }
+
+    // check for or clause
+    val filter2 = filterBitSetOrMapBC.value.forall({ case (name, filterBitSets) =>
+      val dataBitSet = deserializeBitSet(row.get(row.fieldIndex(name)).asInstanceOf[Array[Byte]])
+      filterBitSets.exists({ filterBitSet =>
+        // set the bits from first condition, then xor to clear them. Check if bitset is 
+        val bitSet = new BitSet()
+        bitSet.or(filterBitSet)
+        bitSet.and(dataBitSet)
+        bitSet.cardinality() == filterBitSet.cardinality()
+      })
+    })
+
+    filter2
+  }
+
+  /**
+   * clear the broadcast variables.
+   */
+  def unpersist(): Unit = {
+    filterBitSetAndMapBC.unpersist()
+    filterBitSetOrMapBC.unpersist()
+  }
+}
+
