fb806bdee6ca1322e99012043c5ac371a6022b1e
Amit Jaiswal <amit.jaiswal@crowdstrike.com>
2023-11-21 10:32:30 -0800

    Refactoring

---
+++ projects/hydra/scripts/get-alive-wf-ids.py
@@ -0,0 +1,7 @@
+from omigo_hydra import cluster_services_v2
+import sys
+from omigo_core import utils
+utils.disable_info_mode()
+
+capabilities = sys.argv[1].split(",")
+cluster_services_v2.print_alive_wf_ids(capabilities)
