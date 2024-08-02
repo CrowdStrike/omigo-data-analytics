1c231cb406a96c50fca39aa5fb041d2d160cf41e
Amit Jaiswal <amit.jaiswal@crowdstrike.com>
2024-08-02 12:29:07 -0700

    version

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cd python-packages/core
 python3 -m build
 
 echo "Installing core package"
-pip3 install dist/omigo_core-0.7.4.tar.gz
+pip3 install dist/omigo_core-0.7.5.tar.gz
 
 echo "Building extensions package"
 cd -
@@ -16,7 +16,7 @@ cd python-packages/extensions
 python3 -m build
 
 echo "Installing extensions package"
-pip3 install dist/omigo_ext-0.7.4.tar.gz
+pip3 install dist/omigo_ext-0.7.5.tar.gz
 
 echo "Building hydra package"
 cd -
@@ -24,5 +24,5 @@ cd python-packages/hydra
 python3 -m build
 
 echo "Installing hydra package"
-pip3 install dist/omigo_hydra-0.7.4.tar.gz
+pip3 install dist/omigo_hydra-0.7.5.tar.gz
 
