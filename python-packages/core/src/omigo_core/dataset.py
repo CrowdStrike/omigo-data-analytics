e6b52a805d2af4fc6edf1b942d1dd2daab367996
Amit Jaiswal <amit.jaiswal@crowdstrike.com>
2024-09-22 17:13:58 -0700

    update

---
+++ python-packages/core/src/omigo_core/dataset.py
@@ -0,0 +1,6 @@
+from omigo_core import tsv, utils
+
+# class for general purpose dataset that has meta information, encoding rules, prior knowledge
+class Dataset:
+    def __init__(self):
+        pass
